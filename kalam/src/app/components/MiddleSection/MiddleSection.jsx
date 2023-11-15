fb812f6dd200641e2b0acce7bfc71669e25c47d2
Saksham <sakshamsingh5001@gmail.com>
2023-11-16 00:02:54 +0530

    Created Search component and Middle Section Header

--- kalam/src/app/components/MiddleSection/MiddleSection.jsx
+++ kalam/src/app/components/MiddleSection/MiddleSection.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import MiddleSectionStyles from './MiddleSection.module.css';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { IconButton, Typography } from '@mui/material';
+import Search from '../Search/Search';
+import Card from '../Card/Card';
+import { Lato } from 'next/font/google';
+
+const lato=Lato({
+    subsets: ['latin-ext'],
+    weight: ['400', '700'],
+})
 
 const MiddleSection = () => {
     return (
         <div className={MiddleSectionStyles.middleContainer}>
-        
+            <div className={MiddleSectionStyles.header}>
+                <div className="moveBtns">
+                    <IconButton>
+                        <KeyboardArrowLeftIcon className={MiddleSectionStyles.arrowBtn}/>
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardArrowRightIcon className={MiddleSectionStyles.arrowBtn}/>
+                    </IconButton>
+                </div>
+                <Search/>
+                <div className='dots'>
+                    <IconButton>
+                        <MoreHorizIcon sx={{fontSize:"3vw",marginTop:"3vw"}} className={MiddleSectionStyles.dots}/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className={MiddleSectionStyles.middle}>
+            <img className={MiddleSectionStyles.image} src='albumPhoto.jpeg'></img>
+            </div>
+            <div className={MiddleSectionStyles.welcome}>
+                <div className={MiddleSectionStyles.welcomeHeadings}>
+                <Typography className={lato.className} variant='h4'>Hello, Thomas</Typography>
+                <Typography className={lato.className} variant='h6'>See All</Typography>
+                </div>
+                <Card/>
+            </div>
+            <div className={MiddleSectionStyles.newReleases}></div>
         </div>
     )
 }
--- kalam/src/app/components/Search/Search.jsx
+++ kalam/src/app/components/Search/Search.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import searchStyles from './Search.module.css';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from "@mui/icons-material/Search";
+import { TextField } from '@mui/material';
 
 const Search = () => {
     return (
-        <div>
-            
+        <div className={searchStyles.searchContainer}>
+            <TextField
+            className={searchStyles.search}
+  label="Search for artists, songs, or albums"
+  InputProps={{
+    endAdornment: (
+      <InputAdornment>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </InputAdornment>
+    )
+  }}
+/>
         </div>
     )
 }
